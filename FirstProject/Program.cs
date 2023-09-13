221598e8c215dc13fcf988b23410f6b6922cdf4b
sumer shakil <sumerahmed1709@gmail.com>
2023-09-13 07:58:36 +0530

    13-9-2023 class

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This is a comment. 
+//Following line is used for printing in console.
+Console.WriteLine("Enter you name");
+string name = Console.ReadLine();//Saving in variable
+Console.WriteLine("This is a .net batch");
+Console.Write("I am a .net Teacher. ");
+Console.Write("My name is ");
+Console.Write(name);
+
+//Ask user to enter his name, surname, age, gender, address, city, phone no 
+//save these details in variable.
+//print all the details in separate line

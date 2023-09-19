22102f7e64b873b2210474199645e9e3d68671bb
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2023-09-19 08:04:05 +0530

    update

---
+++ SecondProject/Program.cs
@@ -0,0 +1,7 @@
+﻿string name = "Hello world from Second Project";
+
+
+Console.WriteLine(name);
+Console.WriteLine("We are Student of .Net Class");
+Console.Write("Nass");
+Console.Write("Technologies");

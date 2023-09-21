94bad5a69443656f7adb03763d9b8fa59e1e517e
tanzi <tanzi@LAPTOP-V2CAT15J>
2023-09-21 07:59:59 +0530

    update

--- SecondProject/Program.cs
+++ SecondProject/Program.cs
@@ -1,7 +1,14 @@
-﻿string name = "Hello world from Second Project";
-
+﻿Console.WriteLine("Enter your name");
+string name = Console.ReadLine();
+Console.WriteLine("Enter your surname");
+string surname = Console.ReadLine();
 
 Console.WriteLine(name);
+
+string sentence = "Hello world from Second Project";
+
+
+Console.WriteLine(sentence);
 Console.WriteLine("We are Student of .Net Class");
 Console.Write("Nass");
 Console.Write("Technologies");

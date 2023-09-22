49874dc4128d00c106e7e08620018d19f23f9c77
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2023-09-22 08:05:13 +0530

    22/9/23 update

--- SecondProject/Program.cs
+++ SecondProject/Program.cs
@@ -1,14 +1,3 @@
-﻿Console.WriteLine("Enter your name");
-string name = Console.ReadLine();
-Console.WriteLine("Enter your surname");
-string surname = Console.ReadLine();
+﻿using SecondProject;
 
-Console.WriteLine(name);
-
-string sentence = "Hello world from Second Project";
-
-
-Console.WriteLine(sentence);
-Console.WriteLine("We are Student of .Net Class");
-Console.Write("Nass");
-Console.Write("Technologies");
+Printing.FunctionA();

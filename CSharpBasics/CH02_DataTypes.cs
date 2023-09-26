c48cd0842a9c9bac74097477ec3e333d57acb9fc
sumer shakil <sumerahmed1709@gmail.com>
2023-09-26 07:56:12 +0530

    update

---
+++ CSharpBasics/CH02_DataTypes.cs
@@ -0,0 +1,46 @@
+﻿namespace CSharpBasics;
+/// <summary>
+/// Data Types are the types of data that are stored in RAM memory
+/// Basic Data types are 
+/// 1. integer (signed & unsigned)
+/// 2. real/floating (signed & unsigned)
+/// 3. character
+/// 4. boolean
+/// </summary>
+public class CH02_DataTypes
+{
+    public static void Test()
+    {
+        IntegerDataType();
+    }
+    public static void NumberSystem()
+    {
+        ///There are basically four number system
+        ///1. Decimal
+        ///2. Binary
+        ///3. Octal
+        ///4. Hexadecimal
+        ///In decimal numbers available are 0,1,2,3,4,5,6,7,8,9
+        ///In binary numbers available are 0,1
+        ///Binary(2 bits i.e. 0 & 1)
+        ///000  0
+        ///001  1
+        ///010  2
+        ///011  3
+        ///100  4
+        ///101  5
+        ///110  6
+        ///111  7
+        ///Decimal = 2^(binary bits)
+        ///2^3 = 8
+        ///2^4 = 16
+        ///2^8 = 256 (8bit)
+        ///00000000 = 0
+        ///Homework: 16 bit, 32 bit, 64 bit
+    }
+    public static void IntegerDataType()
+    {
+        ///Integer Data Types contains numbers like
+        ///1, 30, 400, 0, -34, -5000
+    }
+}

0610c4f962a604d8ef208456cd84a96e28443b18
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2023-10-11 07:57:44 +0530

    update

---
+++ CSharpBasics/CH03_TypeConversion.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpBasics;
+/// <summary>
+/// Type conversion is of two types (Valued Datatypes)
+/// 1. Implicit - Automatic (the conversion that does not result in any loss are converted
+/// automatically.)
+/// 2. Explicit - Manual (the conversion that has a chance of any loss are not converted 
+/// automatically and needs to be converted manually)
+/// </summary>
+public class CH03_TypeConversion
+{
+    public static void Test()
+    {
+        ImplicitConversion();
+    }
+    public static void ImplicitConversion()
+    {
+        byte b = 255;       //8bit
+        short s = b;        //16bit
+        int i = s;          //32bit
+        long l = i;         //64bit
+        Int128 int128 = l;  //128bit
+
+        float f = i;
+        double d = i;
+        decimal m = i;
+
+        char c = 'a';       //'a' is 97
+        i = c;              //int32
+        l = c;              //long64
+        int128 = c;         //Int128
+    }
+}

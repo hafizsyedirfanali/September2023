7902a4015658b73d200d2ef66a5ae9a7cd97d6a2
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2023-10-18 08:00:43 +0530

    update

--- CSharpBasics/CH01_StringManipulation.cs
+++ CSharpBasics/CH01_StringManipulation.cs
@@ -2,14 +2,17 @@
 /// <summary>
 /// String Manipulation
 /// 1. Concatenation
-/// 2. Splitting - to be completed after arrays
+/// 2. Splitting - to be completed after arrays and loops
 /// 3. Substring
+/// 4. Trimming
+/// 5. Equals Method is used to compare two strings and returns boolean value
+/// 6. Upper case and Lower case
 /// </summary>
 public class CH01_StringManipulation
 {
     public static void Test()
     {
-        Splitting();
+        UpperLowerCases();
     }
     public static void Concatenation()
     {
@@ -21,23 +24,63 @@ public class CH01_StringManipulation
         string name = Console.ReadLine();
         Console.WriteLine("Enter your surname");
         string surname = Console.ReadLine();
-        Console.WriteLine(surname +  " " + name);//Associativity is Left to Right
+        Console.WriteLine(surname + " " + name);//Associativity is Left to Right
 
 
         //Placeholder Method
         Console.WriteLine("-----------Placeholder Method------------");
-        Console.WriteLine("{0} {1}",name,surname);
+        Console.WriteLine("{0} {1}", name, surname);
 
         //Interpolation Method
         Console.WriteLine("-----------Interpolation Method------------");
         Console.WriteLine($"First Name = {name} Surname = {surname}");
+    }
+    public static void UpperLowerCases()
+    {
+        string upperCamelCase = "HelloWorld";
+        string lowerCamelCase = "helloWorld";
+        string upperCase = "HELLOWORLD";
+        string lowerCase = "helloworld";
+        //Converting given string to UpperCase
+        Console.WriteLine($"Upper case: {lowerCase.ToUpper()}");
+        //Converting given string to LowerCase
+        Console.WriteLine($"Lower case: {upperCase.ToLower()}");
+        //Applications: Case Insensitive comparasions like: captcha
+        string captcha = "Ab12cD";
+        Console.WriteLine("Enter captcha");
+        string enteredCaptcha = Console.ReadLine();
+        bool isMatching = captcha.ToLower().Equals(enteredCaptcha.ToLower());
+        Console.WriteLine($"Authenticated:{isMatching}");
+        //Hw: Ask user to enter username and password
+        //Compare them with stored username and password
+        //Make use of trim and make the authentication case insensitive
+    }
+    public static void Trimming()
+    {
+        //Trimming is a method of removing extra characters from both the 
+        //ends of string. the special characters may be
+        //blank space \b, a tab \t, a new line character \n
+        //Application: It is used in Usernames Passwords, etc
+        //or wherever we compare two string 
+        string name = "\tAsif \n";
+        string trimmedName = name.Trim();
+        //Application
+        string password = "abcd1234";
+        Console.WriteLine("Enter your password:");
+        var enteredPassword = Console.ReadLine();
+        bool isSame = password.Equals(enteredPassword.Trim());
+        Console.WriteLine($"Authenticated:{isSame}");
     }
     public static void Splitting()//Important
     {
         string str = "Hello world";
         string[] strArray = str.Split(' ');
         //we can split with space, ',', '.' or any special characters
-        //HW : Split a string which has atleast 10 names seperated by comma. split it by comma
+        //HW : Split a string which has atleast 10 names seperated by comma.
+        //split it by comma
+        string names = "Abid, Asif, Rehan, Jamil, Akash";
+        string[] namesArray = names.Split(",");
+        Console.WriteLine(namesArray[0]+","+ namesArray[1]);
     }
     public static void Substring()
     {
@@ -45,7 +88,7 @@ public class CH01_StringManipulation
         ///
         string str = "This is a .net class";
         //starting index of . is 10 and with four characters we can get .net
-        string subString = str.Substring(10, 4);
+        string subString = str.Substring(startIndex:10,length:4);
         Console.WriteLine(subString);
         //using IndexOf
         subString = str.Substring(str.IndexOf('.'), 4);

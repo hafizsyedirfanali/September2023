a269f65cce64784b792d1fca0e241ec376b4151d
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2023-10-20 07:57:52 +0530

    update

---
+++ CSharpBasics/CH04_Operators.cs
@@ -0,0 +1,38 @@
+﻿namespace CSharpBasics;
+/// <summary>
+/// Operators are used to perform certain operations on the Operands/Literals.
+/// There are various operators.
+/// 1. Assignment
+/// 2. Arithmatic
+/// 3. Logical
+/// 4. Comparasion
+/// 5. Shift
+/// </summary>
+public class CH04_Operators
+{
+    public static void Test()
+    {
+        AssignmentOperators();
+    }
+    public static void LogicalOperators()
+    {
+        //Logical operators are AND OR NOT
+    }
+    public static void AssignmentOperators()
+    {
+        ///Assignment operators are used to assign values or results from right to left
+        int age = 10;//here = is assignment operator which assign value from Right to Left
+        //for performing arithmtic operation with assignment
+        //for example age = age + 1
+        age += 1;//age = age + 1
+        age -= 1;//age = age - 1
+        age *= 2; //age = age * 2;
+        //age = age /(divide by) 3 and result is quotient
+        age = 20;//if age is 20 then quotient will be 6
+        age /= 3;//age = age / 3; age will have value = 6
+        age = 10;
+        //age = age (divide by) 3 then remainder will be 1
+        age %= 3; //age = age % 3;
+        ///other operators can be written in the same format
+    }
+}
--- CSharpBasics/Program.cs
+++ CSharpBasics/Program.cs
@@ -1,4 +1,4 @@
 ﻿//Code always executes in Program.cs
 using CSharpBasics;
 
-CH01_StringManipulation.Test();
+CH04_Operators.Test();

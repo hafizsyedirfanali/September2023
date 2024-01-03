c986e51f564606d835b24438c622725f0581e10d
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2024-01-03 08:04:29 +0530

    update

--- MVCProject/Program.cs
+++ MVCProject/Program.cs
@@ -10,19 +10,34 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 
 builder.Services.AddDbContextPool<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
     //options.SignIn.RequireConfirmedAccount = true;
     //options.SignIn.RequireConfirmedPhoneNumber = true;
+    //options.Password.RequiredLength = 3;
+    //options.Password.RequireNonAlphanumeric = false;
+    //options.Password.RequireDigit = false;
+    //options.Password.RequireLowercase = false;
+    //options.Password.RequireUppercase = false;
+    //options.Password.RequiredUniqueChars = 0;
+    //options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromDays(1);
+    //options.Lockout.MaxFailedAccessAttempts = 3;
+    //options.Lockout.AllowedForNewUsers = false;
 }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
 
 builder.Services.AddMvc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. FIFO
+//in pipeline sequence of operation is very important. Operations must be in correct sequence.
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -35,9 +50,12 @@ else
 }
 
 app.UseHttpsRedirection();
+
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 

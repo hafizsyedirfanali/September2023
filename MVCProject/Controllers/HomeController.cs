2ad902763a19fb68fb7aa9b76ae2db58bb77073d
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2024-01-09 08:04:01 +0530

    update

--- MVCProject/Controllers/HomeController.cs
+++ MVCProject/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MVCProject.Models;
 using System.Diagnostics;
@@ -12,7 +13,6 @@ namespace MVCProject.Controllers
         {
             _logger = logger;
         }
-
         public IActionResult Index()
         {
             //return NotFound();

86e57282cdfa563452220ddcba392b30708ffd68
Suboor shaikh <Suboor shaikh@LAPTOP-U202SUFC>
2024-01-10 08:01:48 +0530

    update

---
+++ MVCProject/Controllers/DataController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class DataController : Controller
+    {
+        public IActionResult ViewDataAction()
+        {
+            ViewData["WelcomeText"] = "Hello from Irfan Sir. Its cold today";
+            return View();
+        }
+    }
+}
